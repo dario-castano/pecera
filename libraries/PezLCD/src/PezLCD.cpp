83f508fa22e00d408782547ce184ffe4014897c6
Dario Castano <hdcastano@outlook.com>
2022-05-30 16:24:36 -0500

    Agrega constructor y destructor pezlcd

--- libraries/PezLCD/src/PezLCD.cpp
+++ libraries/PezLCD/src/PezLCD.cpp
@@ -2,9 +2,29 @@
  * PezLCD.cpp
  *
  *  Created on: May 30, 2022
- *      Author: dario
+ *      Author: Un pescado
  */
+#include "PezLCD.h"
+#include <LiquidCrystal.h>
+#include <Arduino.h>
 
+// Constructor del objeto
+PezLCD::PezLCD(LiquidCrystal* lcd) {
+	this->lcd = lcd;
+	this->tempBuf = (char*)malloc(sizeof(char) * 9);
+	this->luzBuf = (char*)malloc(sizeof(char) * 8);
+	this->nivelBuf = (char*)malloc(sizeof(char) * 9);
+	this->tareaBuf = (char*)malloc(sizeof(char) * 8);
+	this->tempValue = 0;
+	this->luzMode = 0;
+	this->nivelMode = 0;
+	this->tarea = 0;
+}
 
-
-
+// Libera los buffer de la memoria
+PezLCD::~PezLCD() {
+	free(tempBuf);
+	free(luzBuf);
+	free(nivelBuf);
+	free(tareaBuf);
+}
--- libraries/PezLCD/src/PezLCD.h
+++ libraries/PezLCD/src/PezLCD.h
@@ -7,10 +7,15 @@ class PezLCD {
 	private:
 		LiquidCrystal* lcd;
 
-		char* temp_buf;
-		char* luz_buf;
-		char* nivel_buf;
-		char* tarea_buf;
+		int tempValue;
+		int luzMode;
+		int nivelMode;
+		int tarea;
+
+		char* tempBuf;
+		char* luzBuf;
+		char* nivelBuf;
+		char* tareaBuf;
 
 		const byte CURSOR_TEMP_COL = 0;
 		const byte CURSOR_TEMP_ROW = 0;
@@ -25,6 +30,7 @@ class PezLCD {
 		const char LUZ_MSG[5] = "LUZ:";
 		const char NIVEL_MSG[7] = "NIVEL:";
 		const char TAREA_MSG[6] = "TAREA";
+		const char ERROR_MSG[6] = "ERROR";
 
 		const char LUZ_MSG_ON[3] = "ON";
 		const char LUZ_MSG_OFF[4] = "OFF";
@@ -32,6 +38,10 @@ class PezLCD {
 		const char NIVEL_MSG_OK[3] = "OK";
 		const char NIVEL_MSG_LOW[3] = "LO";
 		const char NIVEL_MSG_HIGH[3] = "HI";
+
+		const char FOUR_EMPTY_SPACES[5] = "    ";
+		const char THREE_EMPTY_SPACES[4] = "   ";
+		const char TWO_EMPTY_SPACES[3] = "  ";
 
 		const char TAREA_NIVEL_AGUA = "10";
 		const char TAREA_LLENAR_AGUA = "11";
@@ -42,6 +52,17 @@ class PezLCD {
 		const char TAREA_LEER_TERMOMETRO = "30";
 		const char ERROR_TERMO_FUERA_RANGO = "99";
 
+	public:
+		PezLCD(LiquidCrystal* lcd);
+		~PezLCD();
+
+		void setTemp(int tempValue);
+		void setLuz(int luzMode);
+		void setNivel(int nivelMode);
+		void setTarea(int tarea);
+
+		void print();
+		void clear();
 };
 
 #endif
