c6cc441670113313e40a1f993b37341ed83eb529
Dario Castano <hdcastano@outlook.com>
2022-05-30 18:16:01 -0500

    Agrega prueba libreria pez_lcd

--- pecera.ino
+++ pecera.ino
@@ -1,14 +1,50 @@
 #include "Arduino.h"
 #include "PezLCD.h"
 
-//The setup function is called once at startup of the sketch
+PezLCD lcd;
+const byte LCD_PIN_RS = 12;
+const byte LCD_PIN_E = 11;
+const byte LCD_PIN_D4 = 5;
+const byte LCD_PIN_D5 = 4;
+const byte LCD_PIN_D6 = 3;
+const byte LCD_PIN_D7 = 2;
+
 void setup()
 {
-// Add your initialization code here
+	lcd = PezLCD(
+			LCD_PIN_RS,
+			LCD_PIN_E,
+			LCD_PIN_D4,
+			LCD_PIN_D5,
+			LCD_PIN_D6,
+			LCD_PIN_D7);
+
 }
 
-// The loop function is called in an endless loop
 void loop()
 {
-//Add your repeated code here
+	lcd.setTemp(20);
+	lcd.setLuz(0);
+	lcd.setNivel(0);
+	lcd.setTarea(99);
+	lcd.print();
+
+	delay(2000);
+	lcd.clear();
+
+	lcd.setTemp(76);
+	lcd.setLuz(1);
+	lcd.setNivel(-1);
+	lcd.setTarea(20);
+	lcd.print();
+
+	delay(2000);
+
+	lcd.setTemp(23);
+	lcd.setLuz(1);
+	lcd.setNivel(0);
+	lcd.setTarea(10);
+	lcd.print();
+
+	delay(2000);
 }
